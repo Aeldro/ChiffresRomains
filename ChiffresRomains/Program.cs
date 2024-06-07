ef80cc199627f6444d2c660cf6a04ff6ac45d7c8
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-07 10:48:29 +0200

    Ajoutez des fichiers projet.

---
+++ ChiffresRomains/Program.cs
@@ -0,0 +1,140 @@
+﻿namespace ChiffresRomains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                int result = 0;
+                List<int> list = new List<int>();
+
+                static bool isOrdered(List<int> listOfNumbers)
+                {
+                    for (int i = 0; i < listOfNumbers.Count - 1; i++)
+                    {
+                        if (listOfNumbers[i] < listOfNumbers[i + 1])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+
+                static bool isMoreThanTreeReps(List<int> listOfNumbers)
+                {
+                    for (int i = 0; i < listOfNumbers.Count; i++)
+                    {
+                        if (listOfNumbers[i] == 1 || listOfNumbers[i] == 10 || listOfNumbers[i] == 100 || listOfNumbers[i] == 1000)
+                        {
+                            if (i < listOfNumbers.Count - 3 && listOfNumbers[i] == listOfNumbers[i + 1] && listOfNumbers[i + 1] == listOfNumbers[i + 2] && listOfNumbers[i + 2] == listOfNumbers[i + 3])
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            if (i < listOfNumbers.Count - 1 && listOfNumbers[i] == listOfNumbers[i + 1])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                }
+                Console.WriteLine("Entrez un chiffre romain.");
+                string romanNumber = Console.ReadLine();
+
+                for (int i = 0; i < romanNumber.Length; i++)
+                {
+                    if (romanNumber[i] == 'M')
+                    {
+                        list.Add(1000);
+                    }
+                    else if (romanNumber[i] == 'D')
+                    {
+                        list.Add(500);
+                    }
+                    else if (romanNumber[i] == 'C')
+                    {
+                        if (i < romanNumber.Length - 1 && romanNumber[i + 1] == 'D')
+                        {
+                            list.Add(400);
+                            i++;
+                        }
+                        else if (i < romanNumber.Length - 1 && romanNumber[i + 1] == 'M')
+                        {
+                            list.Add(900);
+                            i++;
+                        }
+                        else
+                        {
+                            list.Add(100);
+                        }
+                    }
+                    else if (romanNumber[i] == 'L')
+                    {
+                        list.Add(50);
+                    }
+                    else if (romanNumber[i] == 'X')
+                    {
+                        if (i < romanNumber.Length - 1 && romanNumber[i + 1] == 'L')
+                        {
+                            list.Add(40);
+                            i++;
+                        }
+                        else if (i < romanNumber.Length - 1 && romanNumber[i + 1] == 'C')
+                        {
+                            list.Add(90);
+                            i++;
+                        }
+                        else
+                        {
+                            list.Add(10);
+                        }
+                    }
+                    else if (romanNumber[i] == 'V')
+                    {
+                        list.Add(5);
+                    }
+                    else if (romanNumber[i] == 'I')
+                    {
+                        if (i < romanNumber.Length - 1 && romanNumber[i + 1] == 'V')
+                        {
+                            list.Add(4);
+                            i++;
+                        }
+                        else if (i < romanNumber.Length - 1 && romanNumber[i + 1] == 'X')
+                        {
+                            list.Add(9);
+                            i++;
+                        }
+                        else
+                        {
+                            list.Add(1);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Chiffre romain incorrect.");
+                    }
+                }
+
+                if (isOrdered(list) && isMoreThanTreeReps(list))
+                {
+                    foreach (int item in list)
+                    {
+                        result += item;
+                    }
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Chiffre romain incorrect.");
+                }
+            }
+
+        }
+    }s
+}
